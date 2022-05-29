a765d95ea11c784a87a96a070b8697661f677b24
soufianeel18 <soufiane.elbakkali18@gmail.com>
2022-05-29 20:50:28 +0100

    Update project

---
+++ vendor/spatie/icalendar-generator/src/ComponentPayload.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Spatie\IcalendarGenerator;
+
+use Closure;
+use DateTimeInterface;
+use Exception;
+use Spatie\IcalendarGenerator\Components\Component;
+use Spatie\IcalendarGenerator\PropertyTypes\DateTimePropertyType;
+use Spatie\IcalendarGenerator\PropertyTypes\PropertyType;
+use Spatie\IcalendarGenerator\PropertyTypes\TextPropertyType;
+
+final class ComponentPayload
+{
+    /** @var string */
+    private $type;
+
+    /** @var array */
+    private $properties = [];
+
+    /** @var array */
+    private $subComponents = [];
+
+    public static function create(string $type): ComponentPayload
+    {
+        return new self($type);
+    }
+
+    public function __construct(string $type)
+    {
+        $this->type = $type;
+    }
+
+    public function property(PropertyType $property, array $parameters = null): ComponentPayload
+    {
+        $property->addParameters($parameters ?? []);
+
+        $this->properties[] = $property;
+
+        return $this;
+    }
+
+    /**
+     * @param array|string $names
+     * @param \DateTimeInterface|null $value
+     * @param bool $withTime
+     * @param bool $withTimeZone
+     *
+     * @return \Spatie\IcalendarGenerator\ComponentPayload
+     */
+    public function dateTimeProperty(
+        $names,
+        ?DateTimeInterface $value,
+        bool $withTime = false,
+        bool $withTimeZone = false
+    ): ComponentPayload {
+        if ($value === null) {
+            return $this;
+        }
+
+        return $this->property(new DateTimePropertyType($names, $value, $withTime, $withTimeZone));
+    }
+
+    /**
+     * @param array|string $names
+     * @param string|null $value
+     *
+     * @param bool $disableEscaping
+     *
+     * @return \Spatie\IcalendarGenerator\ComponentPayload
+     */
+    public function textProperty(
+        $names,
+        ?string $value,
+        bool $disableEscaping = false
+    ): ComponentPayload {
+        if ($value === null) {
+            return $this;
+        }
+
+        return $this->property(new TextPropertyType($names, $value, $disableEscaping));
+    }
+
+    public function subComponent(Component ...$components): ComponentPayload
+    {
+        foreach ($components as $component) {
+            $this->subComponents[] = $component;
+        }
+
+        return $this;
+    }
+
+    public function when(bool $condition, Closure $closure): ComponentPayload
+    {
+        if ($condition) {
+            $closure($this);
+        }
+
+        return $this;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getProperties(): array
+    {
+        return $this->properties;
+    }
+
+    public function getProperty(string $name): PropertyType
+    {
+        $filteredProperties = array_filter(
+            $this->properties,
+            function (PropertyType $property) use ($name) {
+                return in_array($name, $property->getNames());
+            }
+        );
+
+        $properties = array_values($filteredProperties);
+
+        if (count($properties) === 0) {
+            throw new Exception("Property `{$name}` does not exist in the payload");
+        }
+
+        return $properties[0];
+    }
+
+    public function getSubComponents(): array
+    {
+        return $this->subComponents;
+    }
+}
